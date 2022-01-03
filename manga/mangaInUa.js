7d9c6565e79843a464d2d195b8ba8820c2fb29c6
armatura24 <vivcharenko10@gmail.com>
2022-01-03 14:49:14 +0200

    fasthtmlparser
    sda
    
    asd

--- manga/mangaInUa.js
+++ manga/mangaInUa.js
@@ -1,24 +1,25 @@
-const lb = require('fast-html-dom-parser');
 
-const FstHtmlPrsr = lb.FastHTMLParser;
 
 function mapToConcreteView(html) {
 
-    
+
 }
 
 function mapToGalleryView(html) {
-    return JSON.stringify(new FstHtmlPrsr(html)
-        .getElementsByClassName('movie').slice(0, -1)
-        .map(e => {
-            return {
-                uid: e.getElementsByTagName('h3')[0].getElementsByTagName('a')[0]
-                    .getAttribute('href').split('/')[5].split('-')[0],
-                title: e.getElementsByTagName('h3')[0].getElementsByTagName('a')[0].textContent,
-                cover: getHost() + e.getElementsByTagName('figure')[0]
-                    .getElementsByTagName('img')[0].getAttribute('data-src')
-            }
-        }));
+
+    return JSON.stringify(
+        new (require('fast-html-dom-parser')).FastHTMLParser(html)
+            .getElementsByClassName('movie').slice(0, -1)
+            .map(e => {
+                return {
+                    uid: e.getElementsByTagName('h3')[0].getElementsByTagName('a')[0]
+                        .getAttribute('href').split('/')[5].split('-')[0],
+                    title: e.getElementsByTagName('h3')[0].getElementsByTagName('a')[0].textContent,
+                    cover: getHost() + e.getElementsByTagName('figure')[0]
+                        .getElementsByTagName('img')[0].getAttribute('data-src')
+                }
+            }));
+
 }
 
 function getHost() {
